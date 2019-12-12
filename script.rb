52dd9ed99090cab2101ce9b35bea1833d5fac576
Student <example@example.com>
2019-12-12 10:49:44 -0800

    add front end

--- script.rb
+++ script.rb
@@ -1,1 +1,8 @@
 #!/usr/bin/env ruby
+
+require('./lib/coin.rb')
+
+puts "How much change do you have in your pocket (in cents)?"
+cents = gets.chomp.to_i
+counter = Counter.new()
+puts counter.get_change(cents)
