acdec3d126bc45293acd0512eb4d8205dbf18f85
Student <example@example.com>
2019-12-12 10:50:02 -0800

    refactor to single method

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,35 +1,17 @@
 class Counter
-  attr_writer(:cents)
   def initialize
-    @cents = 0
-    @coin_array = [
-      ['quarters', 25],
-      ['dimes', 10],
-      ['nickels', 5],
-      ['pennies', 1]
-    ]
+    @coin_array = [['quarters', 25],['dimes', 10],['nickels', 5],['pennies', 1]]
   end
 
   def get_change(cents)
-    @cents = cents
-    self.output(self.count)
-  end
-
-  def count
-    @coin_array.map { |c| coin(c[1]) }
-  end
-
-  def output(coin_values)
+    value_array = @coin_array.map do |c|
+      coins = (cents/c[1]).floor
+      cents -= coins * c[1]
+      coins
+    end
     result = @coin_array.each_with_index.map do |c,i|
-      (coin_values[i] > 0) ? "#{c[0]}: #{coin_values[i]} " : ''
+      (value_array[i] > 0) ? "#{c[0]}: #{value_array[i]} " : ''
     end
     result.join('')
   end
-
-  def coin(n)
-    coins = (@cents/n).floor
-    @cents -= coins * n
-    coins
-  end
-
 end
